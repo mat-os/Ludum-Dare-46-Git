cc0de6aa14b4ee4cec7b83a4b93c82841ac389a6
mat-os <43048752+mat-os@users.noreply.github.com>
2020-04-20 00:21:52 +0300

    AddAboutMenu

---
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool isHaveSave;
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        StartCoroutine(ExitGameRoutine());
+
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    /*//УБРАТЬ!
+    private void Start()
+    {
+        LoadLevel(2);
+
+    }*/
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        
+        loadingScreen.SetActive(true);
+        
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            
+            yield return null;
+        }
+    }
+
+    IEnumerator ExitGameRoutine()
+    {
+        yield return  new WaitForSeconds(0.8f);
+        
+        Application.Quit();
+    }
+}
