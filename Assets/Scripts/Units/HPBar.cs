e838d3b6724a6cf5592829e7e6fbd1df023a105a
mat-os <43048752+mat-os@users.noreply.github.com>
2020-04-19 21:43:49 +0300

    Fix HPBar and New Lvl Sys

--- Assets/Scripts/Units/HPBar.cs
+++ Assets/Scripts/Units/HPBar.cs
@@ -7,8 +7,33 @@ public class HPBar : MonoBehaviour
 {
     [SerializeField]private TMP_Text HPtext;
 
+    [SerializeField] private Transform HPSliderImage;
+    [SerializeField] private Transform FullHPSliderImage;
+    
     public void UpdateHPBar(float maxHP, float HPAmount)
     {
        HPtext.text = HPAmount + "/" +  maxHP;
+
+       var HPtemp = HPAmount / maxHP;
+       SetFillBar(HPtemp);
+    }
+
+    void Start()
+    {
+        // Default to an empty bar
+        var newScale = this.HPSliderImage.localScale;
+        newScale.x = 0;
+        this.HPSliderImage.localScale = newScale;
+    }
+
+    public void SetFillBar(float fillAmount)
+    {
+        // Make sure value is between 0 and 1
+        fillAmount = Mathf.Clamp01(fillAmount);
+
+        // Scale the fillImage accordingly
+        var newScale = this.HPSliderImage.localScale;
+        newScale.x = this.FullHPSliderImage.localScale.x * fillAmount;
+        this.HPSliderImage.localScale = newScale;
     }
 }
