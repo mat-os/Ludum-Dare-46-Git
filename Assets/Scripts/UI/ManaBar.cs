4d7863d0a79d300ddc918b5594f70c770c21e702
HolyGriefer <56173836+HolyGriefer@users.noreply.github.com>
2020-04-19 15:35:22 +0300

    UI
    Added ManaBar Scene

---
+++ Assets/Scripts/UI/ManaBar.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+
+    private Mana mana;
+    private Image barImage;
+
+    private void Awake()
+    {
+        barImage = transform.Find("Bar").GetComponent<Image>();
+
+        mana = new Mana();
+    }
+
+    private void Update()
+    {
+        mana.Update();
+        barImage.fillAmount = mana.GetManaNormalized();
+
+        /*
+        if (Input.GetKeyDown("space"))
+        {
+            mana.SpendMana(10);
+        }
+        */
+    }
+}
+
+public class Mana
+{
+    public const int MANA_MAX = 100;
+
+    private float manaAmount;
+    private float manaRegenAmount;
+
+    //Set Variables
+    public Mana()
+    {
+        manaAmount = 0;
+        manaRegenAmount = 3f;
+    }
+
+    //Add ManaRegen and Clamp it
+    public void Update()
+    {
+        manaAmount += manaRegenAmount * Time.deltaTime;
+        manaAmount = Mathf.Clamp(manaAmount, 0, MANA_MAX);
+    }
+
+    //Method to spend mana
+    public void SpendMana(int amount)
+    {
+        if(manaAmount >= amount)
+        {
+            manaAmount -= amount;
+        }
+    }
+    //Method to restore mana
+    public void RestoreMana(int amount)
+    {
+        manaAmount += amount;
+    }
+
+    public float GetManaNormalized()
+    {
+        return manaAmount / MANA_MAX;
+    }
+} 
